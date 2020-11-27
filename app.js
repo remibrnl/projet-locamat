7fea0d6b180e5065f0f0457df4254a19117ddd38
Rémi Berniolles <remi.berniolles@gmail.com>
2020-11-27 10:34:17 +0100

    en jade

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
+//app.engine('pug', require('pug').renderFile);
 app.set('view engine', 'jade');
 
 app.use(logger('dev'));
@@ -19,6 +20,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+//app.use(express.static(__dirname + '/node_modules/bootstrap/dist'));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
---
+++ user.js
@@ -0,0 +1,14 @@
+class Users{
+    constructor(Matricule,Nom,Prenom,mail,role,hashmdp){
+        this.Matricule = Matricule
+        this.Nom = Nom
+        this.Prenom = Prenom
+        this.mail = mail
+        this.role = role
+        this.mdp = hashmdp
+    }
+
+
+    
+}
+
