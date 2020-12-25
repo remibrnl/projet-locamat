6f79344caeabedf74196ab50443682adcf4ea26d
Rémi Berniolles <remi.berniolles@gmail.com>
2020-12-25 19:23:33 +0100

    debut admin

--- app.js
+++ app.js
@@ -6,7 +6,8 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var loginRouter = require("./routes/login");
+var loginRouter = require('./routes/login');
+var adminRouter = require('./routes/admin')
 
 var app = express();
 
@@ -25,11 +26,13 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(__dirname + '/node_modules/jquery/dist'));
 app.use(express.static(__dirname + '/node_modules/bootstrap/dist'));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/login', loginRouter);
+app.use('/admin', adminRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/admin.js
+++ routes/admin.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('login', { title: 'Locamat : Login' });
+  res.render('admin', { title: 'Locamat : Administration' });
 });
 
 module.exports = router;
