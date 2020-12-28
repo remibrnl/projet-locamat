dd59fc978874ecb19194ce16b08256942751333a
Rémi Berniolles <remi.berniolles@gmail.com>
2020-12-28 19:43:22 +0100

    pas de ; en js

--- app.js
+++ app.js
@@ -1,48 +1,48 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var fs = require('fs');
+var createError = require('http-errors')
+var express = require('express')
+var path = require('path')
+var cookieParser = require('cookie-parser')
+var logger = require('morgan')
+var fs = require('fs')
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var loginRouter = require('./routes/login');
-var adminRouter = require('./routes/admin');
+var indexRouter = require('./routes/index')
+var usersRouter = require('./routes/users')
+var loginRouter = require('./routes/login')
+var adminRouter = require('./routes/admin')
 
 //var user = require('serialization/user');
 //var device = require('serialization/device');
 
 // data reading
-var users = JSON.parse(fs.readFileSync('serialization/users.json'));
-var devices = JSON.parse(fs.readFileSync('serialization/devices.json'));
-console.log(users);
-console.log(devices);
+var users = JSON.parse(fs.readFileSync('serialization/users.json'))
+var devices = JSON.parse(fs.readFileSync('serialization/devices.json'))
+console.log(users)
+console.log(devices)
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, 'views'))
 
 // JADE
-// app.set('view engine', 'jade');
+// app.set('view engine', 'jade')
 
 // HTML
-app.engine('html', require('ejs').renderFile);
-app.set('view engine', 'html');
+app.engine('html', require('ejs').renderFile)
+app.set('view engine', 'html')
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.static(__dirname + '/node_modules/jquery/dist'));
-app.use(express.static(__dirname + '/node_modules/bootstrap/dist'));
+app.use(logger('dev'))
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(cookieParser())
+app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(__dirname + '/node_modules/jquery/dist'))
+app.use(express.static(__dirname + '/node_modules/bootstrap/dist'))
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/login', loginRouter);
-app.use('/admin', adminRouter);
+app.use('/', indexRouter)
+app.use('/users', usersRouter)
+app.use('/login', loginRouter)
+app.use('/admin', adminRouter)
 
 console.log('listening...')
 
@@ -54,12 +54,12 @@ app.use(function(req, res, next) {
 // error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.message = err.message
+  res.locals.error = req.app.get('env') === 'development' ? err : {}
 
   // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+  res.status(err.status || 500)
+  res.render('error')
 });
 
 module.exports = app;
