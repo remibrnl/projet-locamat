f0d35bafb5f2d90d716cac9add94e8284165fd25
Rémi Berniolles <remi.berniolles@gmail.com>
2020-12-28 18:13:36 +0100

    test app.js

--- app.js
+++ app.js
@@ -3,11 +3,21 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var fs = require('fs');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var loginRouter = require('./routes/login');
-var adminRouter = require('./routes/admin')
+var adminRouter = require('./routes/admin');
+
+//var user = require('serialization/user');
+//var device = require('serialization/device');
+
+// data reading
+var users = JSON.parse(fs.readFileSync('serialization/users.json'));
+var devices = JSON.parse(fs.readFileSync('serialization/devices.json'));
+console.log(users);
+console.log(devices);
 
 var app = express();
 
@@ -33,6 +43,8 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/login', loginRouter);
 app.use('/admin', adminRouter);
+
+console.log('listening...')
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
