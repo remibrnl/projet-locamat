e16227734ede37e7c021ff36225cf06ae057fe0e
Maxime2Fauquembergue <maximefauq@gmail.com>
2021-01-05 19:12:07 +0100

    recherche ok affichage a fix

--- routes/adminUsers.js
+++ routes/adminUsers.js
@@ -1,17 +1,39 @@
+const { request } = require('express')
 var express = require('express')
 var router = express.Router()
 var jwt = require('jsonwebtoken')
+var users = require('../db/users.js')
 
-/* GET home page. */
-/*router.get('/',authenticateToken,(req, res)=>{
-  res.render('admin', { title: 'Locamat : Administration', user: req.user })
-});*/
+
+/*var connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'locamat'
+})*/
 
 router.get('/',authenticateToken,(req,res)=>{
-  res.render('adminUsers',{ title: 'Locamat : Users administration', user: req.user })
+  res.render('adminUsers',{ title: 'Locamat : Users administration', user: req.user , postRequest: req.postRequest, searchUser: req.searchUser})
 })
 
-function authenticateToken(req, res, next) {
+router.post('/',authenticateToken,(req,res)=>{
+  var postRequest = true
+  users.findByID(req.body.id, (err, user) => {
+    if (err) {
+      throw err
+    }
+    console.log('POST')
+    if(user==null){
+      var message = 'wrong user id'
+      console.log('user not found')
+    }
+    else{
+      res.render('adminUsers',{title: 'Locamat : Users administration', searchUser: user, message: message, user: req.user, postRequest: postRequest})
+    }
+  })
+})
+
+function authenticateToken(req, res, next) { 
   const cookieToken = req.cookies 
   const token = cookieToken.token
   
