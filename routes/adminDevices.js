b4d916dac33b35a7fce088fd41f2c126db6772cf
Guillaume Guichard <guillaume.guichard@etu.univ-tours.fr>
2021-01-08 16:11:42 +0100

    Affichage des adminDevices

--- routes/adminDevices.js
+++ routes/adminDevices.js
@@ -5,9 +5,28 @@ var jwt = require('jsonwebtoken')
 var devices = require('../db/devices.js');
 
 //Faire router.get pour que AdminDEvices.html marche
-router.get('/',authenticateToken,(req,res)=>{
-    res.render('adminDevices',{ title: 'Locamat : Materiel administration', user: req.user })
-})
+router.get('/',authenticateToken,(req,res,next)=>{
+
+  devices.findAll((err, result) => {
+    if (err) {
+      next(err);
+    }
+    else{
+    
+      var devicesList = result.map((element) => {
+        var device = Object.assign(element);
+        if ( device.borrowingStartDate !== null && device.borrowingEndDate !== null ){
+          device.borrowingStartDate = element.borrowingStartDate.toLocaleString('fr-FR');
+          device.borrowingEndDate = element.borrowingEndDate.toLocaleString('fr-FR');
+        }
+        return device;
+      });
+      
+      res.render('adminDevices',{ title: 'Locamat : Materiel administration', user: req.user, devicesList: devicesList });
+    };
+  });
+
+});
 
 function authenticateToken(req, res, next) {
     const cookieToken = req.cookies 
